7d043f0040ad465d70bf5d4f5cfad48df7b173bf
huazhicai <936844218@qq.com>
2017-08-07 11:02:42 +0800

    implement spider

--- spider/baike_spider/html_outputer.py
+++ spider/baike_spider/html_outputer.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 # coding=utf-8
+
+
 class HtmlOutputer(object):
-    def collect_data(self, new_data):
-        pass
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
 
     def output_html(self):
-        pass
+        fout = open("output.html", "w")
+        fout.write("<html>")
+        fout.write("<meta charset='utf-8'>")
+        fout.write("<body>")
+        fout.write("<table>")
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td width='25%%'>%s</td>" % data["url"])
+            fout.write("<td width='5%%'>%s</td>" % data["title"].encode("utf-8"))
+            fout.write("<td width='70%%'>%s</td>" % data["summary"].encode("utf-8"))
+            fout.write("</tr>")
+        fout.write("</table>")
+        fout.write("</body>")
+        fout.write("</html>")
